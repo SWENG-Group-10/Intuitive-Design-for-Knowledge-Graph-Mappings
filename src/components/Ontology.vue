cc4506d8ebc86d36ba50d6433953bb71f179b43e
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-15 20:38:31 +0000

    created card for the ontology component

---
+++ src/components/Ontology.vue
@@ -0,0 +1,21 @@
+<template>
+ <v-card
+  flat
+  tile
+  color="red"
+  height="400"
+  >
+    <h3>ONTOLOGY</h3>
+  </v-card>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

2f868777b89b91cfd9a9deb9a9347b6d8ac3ed5c
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-15 20:37:39 +0000

    created blank card for header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card
+  height="70"
+  tile
+  flat
+  color="blue"
+  >
+    <h3>HEADER</h3>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

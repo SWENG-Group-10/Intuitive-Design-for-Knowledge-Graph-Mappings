9ac96fa3211d999c3e924d6a32d2c59895c2fa6e
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-15 20:37:55 +0000

    created blank card for JSONViewer component

---
+++ src/components/JSONViewer.vue
@@ -0,0 +1,22 @@
+<template>
+<v-card
+  flat
+  tile
+  color="yellow"
+  height="400"
+  >
+     <h3>JSON VIEWER</h3>
+  </v-card>
+ 
+</template>
+
+<script>
+export default {
+    data: () => ({
+    }),
+}
+</script>
+
+<style>
+
+</style>

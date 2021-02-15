9738157fece75e09229f1a615a4ce274d6e4d583
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-15 20:38:09 +0000

    created blank card for mapping component

---
+++ src/components/Mapping.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card
+  flat
+  tile
+  color="green"
+  height="100%"
+  >
+    <h3>MAPPING</h3>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

d77f5e28b49ee26a9ce55d1eae883e4d52d0c96a
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-16 22:34:26 +0000

    added public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  transpileDependencies: ["vuetify"],
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/intuitive-design-for-knowledge-graph-mappings/"
+      : "/",
+};

0903b6754ee10265fd643b19c06c1ff22532e10b
David Green <44809262+dgreen8443@users.noreply.github.com>
2021-02-17 22:06:18 +0000

    Adding Comments for future

--- src/components/JSONViewer.vue
+++ src/components/JSONViewer.vue
@@ -5,9 +5,11 @@
     outlined
     height="400"
     >
+    <!-- Wrap the JSON displayer in a read only Vue Container -->
+    <!-- deals with large JSON Schema as well as small -->
         <v-container>
             <v-textarea
-            auto-grow
+            auto-grow 
             readonly
             v-model = "json"
             ></v-textarea>
@@ -18,6 +20,8 @@
 <script>
 export default {
     data: () => ({
+        // Sample JSON for testing purposes
+        // Will add upload functionality later
         json: {
             "id": 1,
             "name": "A green door",
@@ -29,6 +33,7 @@ export default {
         }
     }),
     created(){
+        // delimits the JSON Schema into nicely displayed way
         this.json = JSON.stringify(this.json, null, 2)
     }
 }

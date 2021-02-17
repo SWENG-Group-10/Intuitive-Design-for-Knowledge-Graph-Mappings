d1794066714bdbd9f67f24dada8a7e04dbe53ba5
David Green <44809262+dgreen8443@users.noreply.github.com>
2021-02-17 22:45:55 +0000

    Slight visual changes

--- src/components/Ontology.vue
+++ src/components/Ontology.vue
@@ -1,8 +1,9 @@
 <template>
  <v-card>
-   <object type="text/html" width="100%" height="600px" data="http://visualdataweb.de/webvowl/#opts=doc=0;editorMode=true;#iri=https://raw.githubusercontent.com/SemanGit/SemanGit/master/Documentation/ontology/semangitontology.ttl">
+   <h3>ONTOLOGY</h3>
+   <object type="text/html" width="100%" height="550px" data="http://visualdataweb.de/webvowl/#opts=doc=0;editorMode=true;#iri=https://raw.githubusercontent.com/SemanGit/SemanGit/master/Documentation/ontology/semangitontology.ttl">
   </object>
-    <h3>ONTOLOGY</h3>
+    
   </v-card>
   
 </template>

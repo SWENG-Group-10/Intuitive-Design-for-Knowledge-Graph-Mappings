c902fc7dc61bfcdd588de02b7f722bd93e592178
githubuser123 <ailbhemerriman@gmail.com>
2021-02-17 22:29:26 +0000

    Update Ontology.vue
    Changed Template

--- src/components/Ontology.vue
+++ src/components/Ontology.vue
@@ -1,10 +1,7 @@
 <template>
- <v-card
-  flat
-  tile
-  outlined
-  height="400"
-  >
+ <v-card>
+   <object type="text/html" width="100%" height="600px" data="http://visualdataweb.de/webvowl/#opts=doc=0;editorMode=true;#iri=https://raw.githubusercontent.com/SemanGit/SemanGit/master/Documentation/ontology/semangitontology.ttl">
+  </object>
     <h3>ONTOLOGY</h3>
   </v-card>
   

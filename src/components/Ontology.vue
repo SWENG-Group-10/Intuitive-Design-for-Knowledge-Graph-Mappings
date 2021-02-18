361255314972dfe66f15db036a0395bd0f66d9c8
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-18 16:53:53 +0000

    removed ontology header

--- src/components/Ontology.vue
+++ src/components/Ontology.vue
@@ -1,11 +1,9 @@
 <template>
- <v-card>
-   <h3>ONTOLOGY</h3>
-   <object type="text/html" width="100%" height="400px" data="http://visualdataweb.de/webvowl/#opts=doc=0;editorMode=true;#iri=https://raw.githubusercontent.com/SemanGit/SemanGit/master/Documentation/ontology/semangitontology.ttl">
-  </object>
-    
-  </v-card>
-  
+<v-card>
+    <object type="text/html" width="100%" height="400px" data="http://visualdataweb.de/webvowl/#opts=doc=0;editorMode=true;#iri=https://raw.githubusercontent.com/SemanGit/SemanGit/master/Documentation/ontology/semangitontology.ttl">
+    </object>
+
+</v-card>
 </template>
 
 <script>

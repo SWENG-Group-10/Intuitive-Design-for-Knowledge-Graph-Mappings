ee4b69e1e233156baf01b414c83e492f023eea63
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-25 17:25:22 +0000

    fixed text area component to look a bit nicer

--- src/components/JSONViewer.vue
+++ src/components/JSONViewer.vue
@@ -1,50 +1,44 @@
 <template>
-  <v-card flat tile outlined height="400">
+<v-card flat tile outlined height="400">
     <div class="text-right">
-      <v-btn
-        :loading="loading3"
-        :disabled="loading3"
-        color="blue-grey"
-        class="ma-2 white--text"
-        @click="loader = 'loading3'"
-      >
-        Upload
-        <v-icon right dark> mdi-cloud-upload </v-icon>
-      </v-btn>
+        <v-btn :loading="loading3" :disabled="loading3" color="blue-grey" class="ma-2 white--text" @click="loader = 'loading3'">
+            Upload
+            <v-icon right dark> mdi-cloud-upload </v-icon>
+        </v-btn>
     </div>
 
-    <v-container>
-      <v-textarea auto-grow readonly v-model="json"></v-textarea>
-    </v-container>
-  </v-card>
+    <v-textarea no-resize rounded filled readonly color="transparent" background-color="transparent" v-model="json"></v-textarea>
+</v-card>
 </template>
-
 
 <script>
 export default {
-  data: () => ({
-    json: {
-      id: 1,
-      name: "A green door",
-      price: 12.5,
-      tags: ["home", "green"],
+    data: () => ({
+        json: {
+            id: 1,
+            name: "A green door",
+            price: 12.5,
+            tags: ["home", "green"],
+            test: "test fdgajkhfsdjghadkjghadfkjghbadfkjhgadfg",
+            still: "testing woop",
+            Wow: "yup the pup",
+            Dogs: "I like dogs",
+
+        },
+    }),
+    created() {
+        this.json = JSON.stringify(this.json, null, 2);
     },
-  }),
-  created() {
-    this.json = JSON.stringify(this.json, null, 2);
-  },
 
     watch: {
-      loader () {
-        const l = this.loader
-        this[l] = !this[l]
+        loader() {
+            const l = this.loader
+            this[l] = !this[l]
 
-        setTimeout(() => (this[l] = false), 3000)
+            setTimeout(() => (this[l] = false), 3000)
 
-        this.loader = null
-      },
+            this.loader = null
+        },
     },
-  };
-    
+};
 </script>
-

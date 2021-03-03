b3f2ea845d3526cf4c6153ab10b17e5f4e540b6c
Daniel Grace <daniel.grace44@hotmail.com>
2021-03-03 18:54:28 +0000

    formatting fixes

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,47 @@
 <template>
-  <v-app>
-   <v-dialog v-model = "show">
-      <v-card 
-    :loading="loading"
-    class="mx-auto my-12"
-    max-width="500"
-  >
-    <template slot="progress">
-      <v-progress-linear
-        color="blue"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+<v-app>
+    <v-dialog v-model="show">
+        <v-card :loading="loading" class="mx-auto my-12" max-width="500">
+            <template slot="progress">
+                <v-progress-linear color="blue" height="10" indeterminate></v-progress-linear>
+            </template>
 
-    <v-card-title>Intuitive Design for Knowledge Graph Mappings</v-card-title>
+            <v-card-title>Intuitive Design for Knowledge Graph Mappings</v-card-title>
 
-    <v-divider class="mx-4"></v-divider>
+            <v-divider class="mx-4"></v-divider>
 
-    <v-card-actions>
-      <v-btn
-        color="blue lighten-2"
-        text 
-        @click="Upload"
-      >
-        Upload Ontology
-      </v-btn>
-    </v-card-actions>
+            <v-card-actions>
+                <v-btn color="blue lighten-2" text @click="Upload">
+                    Upload Ontology
+                </v-btn>
+            </v-card-actions>
 
-    <v-card-actions>
-      <v-btn
-        color="blue lighten-2"
-        text 
-        @click="Upload"
-      >
-        Upload JSON file
-      </v-btn>
-    </v-card-actions>
+            <v-card-actions>
+                <v-btn color="blue lighten-2" text @click="Upload">
+                    Upload JSON file
+                </v-btn>
+            </v-card-actions>
 
-    <v-card-actions>
-      <v-btn
-        color="red lighten-2"
-        text 
-        @click= "show = false"
-      >
-        Confirm
-      </v-btn>
-    </v-card-actions>
+            <v-card-actions>
+                <v-btn color="red lighten-2" text @click="show = false">
+                    Confirm
+                </v-btn>
+            </v-card-actions>
 
+        </v-card>
 
-  </v-card>
-
-   </v-dialog>
-    <Header/>
+    </v-dialog>
+    <Header />
     <v-row align-content="start" no-gutters>
-      <v-col>
-        <Mapping/>
-      </v-col>
-      <v-col class="justify-start">
-          <Ontology/>
-          <JSONViewer/>
-      </v-col>
+        <v-col>
+            <Mapping />
+        </v-col>
+        <v-col class="justify-start">
+            <Ontology />
+            <JSONViewer />
+        </v-col>
     </v-row>
-  </v-app>
+</v-app>
 </template>
 
 <script>
@@ -71,30 +50,29 @@ import Ontology from "./components/Ontology"
 import Mapping from "./components/Mapping"
 import Header from "./components/Header"
 
-
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    Mapping,
-    Ontology,
-    JSONViewer,
-    Header
-  },
+    components: {
+        Mapping,
+        Ontology,
+        JSONViewer,
+        Header
+    },
 
     data: () => ({
-      loading: false,
-      selection: 1,
-      show: true,
+        loading: false,
+        selection: 1,
+        show: true,
     }),
 
     methods: {
-      Upload () {
-        this.loading = true
+        Upload() {
+            this.loading = true
 
-        setTimeout(() => (this.loading = false), 2000)
-      },
+            setTimeout(() => (this.loading = false), 2000)
+        },
     },
-  
+
 };
 </script>

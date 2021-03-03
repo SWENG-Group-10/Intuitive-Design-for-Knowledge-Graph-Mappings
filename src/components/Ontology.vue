ead39e985e9991b0bbd4112ea78dbba8b29acdec
David Green <44809262+dgreen8443@users.noreply.github.com>
2021-03-03 16:56:02 +0000

    Revert "fixed ontology to look nice"
    This reverts commit 58e1c315fd6b8dc5a9bb442f04db1a6f6f53c760.

--- src/components/Ontology.vue
+++ src/components/Ontology.vue
@@ -1,6 +1,7 @@
 <template>
  <v-card>
-   <object type="text/html" width="100%" height="400px" data="http://visualdataweb.de/webvowl/#opts=doc=0;editorMode=true;#iri=https://raw.githubusercontent.com/SemanGit/SemanGit/master/Documentation/ontology/semangitontology.ttl">
+   <h3>ONTOLOGY</h3>
+   <object type="text/html" width="100%" height="550px" data="http://visualdataweb.de/webvowl/#opts=doc=0;editorMode=true;#iri=https://raw.githubusercontent.com/SemanGit/SemanGit/master/Documentation/ontology/semangitontology.ttl">
   </object>
     
   </v-card>

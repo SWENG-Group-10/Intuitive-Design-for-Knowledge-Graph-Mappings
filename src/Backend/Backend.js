f712f05f83c49bac7d76460013c884c53af535cf
Daniel Grace <daniel.grace44@hotmail.com>
2021-03-04 16:24:07 +0000

    formatting changes

--- src/Backend/Backend.js
+++ src/Backend/Backend.js
@@ -5,27 +5,21 @@ export default {
     let array = [];
     //var text;
 
-     
-
     const reader = new FileReader();
 
     reader.readAsText(file);
-    
-    
+
     reader.onload = (e) => {
       console.log(e.target.result);
       let jsonOb = JSON.parse(reader.result);
-      for(var key in jsonOb)
-      {
-
-          array.push(jsonOb[key]);
-      }  
-    }
-
+      for (var key in jsonOb) {
+        array.push(jsonOb[key]);
+      }
+    };
 
     console.log(array);
     return array;
-  }, 
+  },
   jsonPrettifier(file) {
     return JSON.stringify(file, null, 2); //Just doing the prettifying of the json file in the backend as opposed to in the vue file - DG xx
   },

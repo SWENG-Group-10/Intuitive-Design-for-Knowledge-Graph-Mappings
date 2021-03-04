1c2b7d6a84ea8fc3006bcc56170b217e614ea37f
DoussotN <78543975+DoussotN@users.noreply.github.com>
2021-03-04 13:35:34 +0000

    changed code for jsonCrawler

--- src/Backend/Backend.js
+++ src/Backend/Backend.js
@@ -1,26 +1,24 @@
 export default {
   jsonCrawler(file) {
     //NATHAN: Code goes here in the jsonCrawler function! You'll need to return an array which ive declared as array so just return that - DG xx
-
+    console.log(file);
     let array = [];
+    var text;
 
+     
 
-    //loading json file into text to make it 
     const reader = new FileReader();
-    reader.onload = e => console.log(e.target.result)
-    let text = reader.readAsText(file);
-    console.log(text);
 
-    //layout for code 
+    reader.readAsText(file);
+    
+    
+    reader.onload = (e) => {
+      console.log(e.target.result);
+      console.log(reader.result);    
+    }
 
-    //going to be something like,
 
-    //for(i in length of JSON file)
-    //{
-    //  .... 
-    //        todo 
-    //}
-
+  
     return array;
   }, 
   jsonPrettifier(file) {

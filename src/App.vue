fa0cf5eaa080e8a140b9147e71899fc7e500c1b6
Ailbhe Merriman <amerrima@tcd.ie>
2021-03-16 11:44:04 +0000

    Update App.vue
    Added header component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -29,9 +29,58 @@
             </v-card-actions>
 
         </v-card>
-
     </v-dialog>
-    <Header />
+
+
+    <v-card
+     height="70"
+    tile
+    flat
+    color="blue"
+    >
+    <h3>HEADER</h3>
+
+    <v-card-actions>
+      
+      <v-btn 
+        color="red"
+        width=180
+      >
+        Subject
+      </v-btn>
+
+      <v-btn
+        color="lime"
+        width=180
+      >
+        Predicate
+      </v-btn>
+
+      <v-btn
+        color="blue lighten-3"
+        width=180
+      >
+        Object
+      </v-btn>
+
+      <v-btn
+        color="yellow lighten-3"
+        width=180
+      >
+        Literal
+      </v-btn>
+      
+      <v-btn
+        color="orange lighten-3"
+        width=180
+      >
+        Type
+      </v-btn>
+
+    </v-card-actions>
+
+  </v-card>
+
     <v-row align-content="start" no-gutters>
         <v-col>
             <Mapping />
@@ -48,7 +97,6 @@
 import JSONViewer from "./components/JSONViewer"
 import Ontology from "./components/Ontology"
 import Mapping from "./components/Mapping"
-import Header from "./components/Header"
 
 export default {
     name: 'App',
@@ -56,8 +104,7 @@ export default {
     components: {
         Mapping,
         Ontology,
-        JSONViewer,
-        Header
+        JSONViewer    
     },
 
     data: () => ({

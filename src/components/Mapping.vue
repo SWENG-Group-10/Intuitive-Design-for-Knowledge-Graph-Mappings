a4fdc9e2a68a9e9546bde7d3bd4bb310e32b78ec
Daniel Grace <daniel.grace44@hotmail.com>
2021-03-29 17:59:22 +0100

    created the toolbox and workstation for blockly to be on

--- src/components/Mapping.vue
+++ src/components/Mapping.vue
@@ -1,20 +1,63 @@
 <template>
-  <v-card
-  flat
-  tile
-  outlined
-  height="800"
-  >
-    <h3>MAPPING</h3>
-  </v-card>
+<v-card flat tile height="800">
+    <BlocklyComponent id="blockly2" :options="options" ref="foo"></BlocklyComponent>
+</v-card>
 </template>
 
 <script>
+import BlocklyComponent from "./BlocklyComponent"
+import '../blocks/stocks'
+import '../prompt'
+import BlocklyJS from 'blockly/javascript';
 export default {
+    name: 'app',
+    components: {
+        BlocklyComponent
+    },
+    data() {
+        return {
+            code: '',
+            options: {
+                media: 'media/',
+                grid: {
+                    spacing: 25,
+                    length: 3,
+                    colour: '#ccc',
+                    snap: true
+                },
+                toolbox: `<xml>
+          <category name="Class" colour="%{BKY_LOGIC_HUE}">
+            <block type="controls_if"></block>
+          </category>
+          <category name="ID" colour="%{BKY_LOOPS_HUE}">
+          </category>
+          <category name="Relation" colour="%{BKY_MATH_HUE}">
+          </category>
+          <category name="Value" colour="%{BKY_VARIABLES_HUE}">
+            </category>
+          <category name="Function" colour="%{BKY_LOOPS_HUE}">
+          </category>
+          <category name="Type" colour="%{BKY_LOOPS_HUE}">
+          </category>
+        </xml>`
+            }
+        }
+    },
+    methods: {
+        showCode() {
+            this.code = BlocklyJS.workspaceToCode(this.$refs["foo"].workspace);
+        }
 
+    }
 }
 </script>
 
 <style>
-
+#blockly2 {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>

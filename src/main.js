7f7ba8a70b4cbe13ecfbd2ddf7d0834cedb1a6a7
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-22 17:50:46 +0100

    Revert "created highlightjs vue directive"
    This reverts commit 72333b2a21a635ef271d156b5bdf8790ba43834d.

--- src/main.js
+++ src/main.js
@@ -1,64 +1,36 @@
-import Vue from "vue";
+import Vue from 'vue'
 // import App from './App.vue'
-import vuetify from "./plugins/vuetify";
-import hljs from "highlight.js";
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false;
+
+Vue.config.productionTip = false
 // const NotFound= { template:'<p>Not found</p>'}
 //const Ttl = { template:"<p>ttl</p>" };
 
-const routes = {
-  "/ttl": "./graph",
-  "/Intuitive-Design-for-Knowledge-Graph-Mappings/ttl": "./graph",
-  "/": "./App",
-};
-
-Vue.directive("highlightjs", {
-  deep: true,
-  bind: function(el, binding) {
-    // on first bind, highlight all targets
-    let targets = el.querySelectorAll("code");
-    targets.forEach((target) => {
-      // if a value is directly assigned to the directive, use this
-      // instead of the element content.
-      if (binding.value) {
-        target.textContent = binding.value;
-      }
-      hljs.highlightBlock(target);
-    });
-  },
-  componentUpdated: function(el, binding) {
-    // after an update, re-fill the content and then highlight
-    let targets = el.querySelectorAll("code");
-    targets.forEach((target) => {
-      if (binding.value) {
-        target.textContent = binding.value;
-        hljs.highlightBlock(target);
-      }
-    });
-  },
-});
+const routes = { 
+  '/ttl': './graph',
+  '/Intuitive-Design-for-Knowledge-Graph-Mappings/ttl': './graph',
+  '/': './App'
+}
 const app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-    currentRoute: window.location.pathname,
+    currentRoute: window.location.pathname
   },
   computed: {
     ViewComponent() {
-      console.log(routes[this.currentRoute]);
-      const matchingView = routes[this.currentRoute];
+      console.log(routes[this.currentRoute])
+      const matchingView = routes[this.currentRoute]
       return matchingView
-        ? require(matchingView + ".vue").default
-        : require("./App.vue").default;
-    },
+        ? require(matchingView + '.vue').default
+        : require ('./App.vue').default
+    }
   },
   vuetify,
   //render: h => h(App)
-  render(h) {
-    return h(this.ViewComponent);
-  },
-}); //.$mount('#app')
+  render (h) {return h(this.ViewComponent)}
+ })//.$mount('#app')
 
-window.addEventListener("popstate", () => {
-  app.currentRoute = window.location.pathname;
-});
+ window.addEventListener('popstate',() =>{
+   app.currentRoute = window.location.pathname
+ })

e7e2700bbd7c13802f4db88d93836afa33c72bab
Daniel Grace <daniel.grace44@hotmail.com>
2021-04-22 14:24:15 +0100

    added helper stepper card

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@
 
                     </v-btn>
 
-                    <v-btn icon  target="_blank" href="https://github.com/SWENG-Group-10/Intuitive-Design-for-Knowledge-Graph-Mappings">
+                    <v-btn icon target="_blank" href="https://github.com/SWENG-Group-10/Intuitive-Design-for-Knowledge-Graph-Mappings">
                         <v-icon> mdi-github </v-icon>
                     </v-btn>
                 </v-col>
@@ -122,30 +122,115 @@
         </v-dialog>
 
         <!-- Dialog Box for Help Page, appears after help button is clicked -->
-        <v-dialog v-model="help" width="500">
+        <v-dialog v-model="help">
             <v-card>
-                <v-card-title class="headline lighten-2">
-                    Help
-                </v-card-title>
-                <v-divider></v-divider>
-                <v-card-text>
-                    Welcome,
-                </v-card-text>
-                <v-card-text>
-                    The top right part of the page displays your ttl file.
-                </v-card-text>
-                <v-card-text>
-                    The bottom right part of the page displays your JSON file.
-                </v-card-text>
-                <v-card-text>
-                    On the left hand side you can find jigsaw pieces to drag and connect.
-                </v-card-text>
-                <v-card-text>
-                    Don't forget you can download your Mapping by pressing the "Download Mapping" button at the bottom of the page.
-                </v-card-text>
-                <v-card-text>
-                    To upload new files please press the return button on the top left of the page.
-                </v-card-text>
+                <v-stepper v-model="e1">
+                    <v-stepper-header>
+                        <v-stepper-step :complete="e1 > 1" step="1">
+                            Ontology Viewer
+                        </v-stepper-step>
+
+                        <v-divider></v-divider>
+
+                        <v-stepper-step :complete="e1 > 2" step="2">
+                            JSON Viewer
+                        </v-stepper-step>
+
+                        <v-divider></v-divider>
+
+                        <v-stepper-step :complete="e1 > 3" step="3">
+                            Mapping Creator
+                        </v-stepper-step>
+
+                        <v-divider></v-divider>
+
+                        <v-stepper-step :complete="e1 > 4" step="4">
+                            Downloading Mappings
+                        </v-stepper-step>
+
+                        <v-divider></v-divider>
+
+                        <v-stepper-step step="5">
+                            Starting Over
+                        </v-stepper-step>
+                    </v-stepper-header>
+
+                    <v-stepper-items>
+                        <v-stepper-content step="1" class="text-center">
+                            <v-card class="mb-2 text-center" color="white" flat>                   
+                                <v-img src="./assets/ontology.png" max-width="1200" class="mx-auto"></v-img>
+                            </v-card>
+                            <h2 class="mb-12">The top right part of the page displays your ttl file on the WebVowl system.</h2>
+                            <v-btn color="blue" dark @click="e1 = 2" class="mr-2">
+                                Continue
+                            </v-btn>
+
+                            <v-btn text @click="help=false">
+                                Cancel
+                            </v-btn>
+                        </v-stepper-content>
+
+                        <v-stepper-content step="2" class="text-center">
+                            
+                            <v-card class="mb-2 text-center" color="white" flat>
+                                <v-img src="./assets/jsonview.png" max-width="1200" class="mx-auto"></v-img>
+                            </v-card>
+                            <h2 class="mb-12">The bottom right part of the page displays your JSON file which you can scroll through.</h2>
+                            <v-btn color="blue" @click="e1 = 3" class="mr-2" dark>
+                                Next
+                            </v-btn>
+
+                            <v-btn text @click="e1 = 1">
+                                Previous
+                            </v-btn>
+                        </v-stepper-content>
+
+                        <v-stepper-content step="3" class="text-center">
+                            
+                            <v-card class="mb-2" color="white" flat>
+                                <v-img src="./assets/mapping.png" max-width="1200" class="mx-auto"></v-img>
+                            </v-card>
+                            <h2 class="mb-12">On the left hand side you can find jigsaw pieces in each of the categories on the left and then use these pieces to create your mappings.</h2>
+                            <v-btn color="blue" @click="e1 = 4" class="mr-2" dark>
+                                Next
+                            </v-btn>
+
+                            <v-btn text @click="e1 = 2">
+                                Previous
+                            </v-btn>
+                        </v-stepper-content>
+
+                        <v-stepper-content step="4" class="text-center">
+                            
+                            <v-card class="mb-2" color="white" flat>
+                                <v-img src="./assets/button.png" max-width="1200" class="mx-auto"></v-img>
+                            </v-card>
+                            <h2 class="mb-12">To download your Mapping by pressing the "Download Mapping" button at the bottom of the page.</h2>
+                            <v-btn color="blue" @click="e1 = 5" class="mr-2" dark>
+                                Next
+                            </v-btn>
+
+                            <v-btn text @click="e1 = 3">
+                                I Got It Thanks!
+                            </v-btn>
+                        </v-stepper-content>
+
+                        <v-stepper-content step="5" class="text-center">
+                            
+                            <v-card class="mb-2" color="white" flat>
+                                <v-img src="./assets/reset.png" max-width="1200" class="mx-auto"></v-img>
+                            </v-card>
+                            <h2 class="mb-12">To upload new files please press the return button on the top left of the page which will bring you back to the original landing page.</h2>
+                            <v-btn color="blue" @click="e1 = 1" class="mr-2" dark>
+                                Start Again
+                            </v-btn>
+
+                            <v-btn text @click="help=false; e1 = 1">
+                                I Got It Thanks!
+                            </v-btn>
+                        </v-stepper-content>
+                    </v-stepper-items>
+                </v-stepper>
             </v-card>
         </v-dialog>
 
@@ -163,7 +248,7 @@
         <!-- Download Button, bottom of page -->
         <v-footer padless color="blue" dark height="100">
             <v-col class="text-center" cols="12">
-                <h1>TCD ADAPT</h1>
+                <v-img src="./assets/adapt.png" max-height="75" max-width="75" class="mx-auto"></v-img>
             </v-col>
         </v-footer>
 
@@ -192,6 +277,7 @@ export default {
         chosen: false,
         ttlChosen: false,
         jsonChosen: false,
+        e1: 1,
         dialog: false, //boolean for return button dialog box
         info: false, //boolean for info button dialog box
         help: false, //boolean for help button dialog box
